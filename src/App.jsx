8a14d589b7a5f21d0749e5aca4cb8e589e112fe2
Anubhav <xcaliber704@gmail.com>
2025-06-27 19:44:57 +0530

    feature/added_tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Canvas from "./components/Canvas"
+import Toolkit from "./components/Toolkit"
 
 const App = () => {
   return (
     <div>
       <Canvas />
+      <Toolkit />
     </div>
   )
 }
--- src/components/Toolkit.jsx
+++ src/components/Toolkit.jsx
@@ -1,9 +1,55 @@
-const Toolkit = () => {
-  return (
-    <div className="w-30 h-full absolute left-0 bg-white mx-10">
-        
-    </div>
-  )
-}
+import { useEffect, useState } from 'react';
+import { MousePointer2, Hand, SquareDashed, Route } from 'lucide-react';
 
-export default Toolkit
+const tools = [
+  { name: 'default', Icon: MousePointer2, cursor: 'default' },
+  { name: 'hand', Icon: Hand, cursor: 'grab' },
+  { name: 'route', Icon: Route, cursor: 'default' },
+  { name: 'square', Icon: SquareDashed, cursor: 'default' }
+];
+
+const Toolkit = () => {
+  const [activeTool, setActiveTool] = useState('default');
+  const [isMouseDown, setIsMouseDown] = useState(false);
+
+  useEffect(() => {
+  const tool = tools.find((t) => t.name === activeTool);
+  if (tool?.name === 'hand' && isMouseDown) {
+    document.body.style.cursor = 'grabbing';
+  } else {
+    document.body.style.cursor = tool?.cursor || 'default';
+  }
+}, [activeTool, isMouseDown]);
+
+useEffect(() => {
+  const handleMouseDown = () => setIsMouseDown(true);
+  const handleMouseUp = () => setIsMouseDown(false);
+
+  window.addEventListener('mousedown', handleMouseDown);
+  window.addEventListener('mouseup', handleMouseUp);
+
+  return () => {
+    window.removeEventListener('mousedown', handleMouseDown);
+    window.removeEventListener('mouseup', handleMouseUp);
+  };
+}, []);
+
+
+  return (
+    <div className="absolute left-1/2 -translate-x-1/2 bottom-4 bg-white flex rounded-md gap-4 p-4 shadow-md shadow-black">
+      {tools.map((tool) => (
+        <tool.Icon
+          key={tool.name}
+          size={30}
+          strokeWidth={1.5}
+          onClick={() => setActiveTool(tool.name)}
+          className={`cursor-pointer text-gray-600 hover:text-purple-800 ${
+            activeTool === tool.name ? 'text-purple-800' : ''
+          }`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Toolkit;
